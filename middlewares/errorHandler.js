c8f21d8172de6b2987396950b24f0df9a06064e6
okami <zabdiel_rosas@outlook.com>
2024-01-07 21:07:37 -0600

    fix(errorHandler): status code assignment validation is improved

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode || 500
+  const statusCode = res.statusCode !== 200 ? res.statusCode : 500
 
   const errorTitle = {
     400: 'Validation Error',
