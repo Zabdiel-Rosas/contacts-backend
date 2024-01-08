d433f220e4d868c425c992ce4a3dd7b83fd3d996
okami <zabdiel_rosas@outlook.com>
2024-01-07 18:33:33 -0600

    add(userModel.js): user schema and model are created

---
+++ models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Please add the username'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add the user email address'],
+    unique: [true, 'Another user has registered this email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add the user password'],
+  },
+})
+
+module.exports = mongoose.model('User', userSchema)
