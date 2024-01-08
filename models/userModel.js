549d790dd0d0d9c87b57f9c6f0ead0f3a53ddc8c
okami <zabdiel_rosas@outlook.com>
2024-01-07 21:08:27 -0600

    fix(userModel.js): time stamp is added to the user schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,19 +1,24 @@
 const mongoose = require('mongoose')
 
-const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, 'Please add the username'],
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'Please add the username'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please add the user email address'],
+      unique: [true, 'Another user has registered this email'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Please add the user password'],
+    },
   },
-  email: {
-    type: String,
-    required: [true, 'Please add the user email address'],
-    unique: [true, 'Another user has registered this email'],
-  },
-  password: {
-    type: String,
-    required: [true, 'Please add the user password'],
-  },
-})
+  {
+    timestamps: true,
+  }
+)
 
 module.exports = mongoose.model('User', userSchema)
