c3086ee9d4c1b4aebc1f135dbfda75576b374efb
okami <zabdiel_rosas@outlook.com>
2024-01-09 19:36:43 -0600

    refactor(server.js): app instance is implemented

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
-const express = require('express')
-const errorHandler = require('./middlewares/errorHandler')
-const connectDb = require('./config/dbConnection')
-const dotenv = require('dotenv').config()
+const getApp = require('./core/index')
 
-connectDb()
-const app = express()
-const port = process.env.PORT || 3000
-
-//middlewares
-app.use(express.json())
-app.use('/api/contacts', require('./routes/contactRoutes'))
-app.use('/api/users', require('./routes/userRoutes'))
-app.use(errorHandler)
+const { app, port } = getApp()
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`)
