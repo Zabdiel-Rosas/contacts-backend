9efb642e6e2dd4a32d4dc751d06f5ca03c5f28ea
okami <zabdiel_rosas@outlook.com>
2024-01-10 19:49:35 -0600

    refactor(server.js): port value is gotten from configuration instance

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const getApp = require('./core/index')
+const config = require('./config/index')
 
-const { app, port } = getApp()
+const app = getApp()
+const port = config.get('app').port
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`)
