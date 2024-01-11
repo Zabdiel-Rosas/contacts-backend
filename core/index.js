3e53fd67d744f74e11ce4db07f4e4b21af35e8e5
okami <zabdiel_rosas@outlook.com>
2024-01-10 19:41:22 -0600

    refactor(core): port is removed and return value is changed to be only app

--- core/index.js
+++ core/index.js
@@ -1,21 +1,18 @@
 const express = require('express')
 const router = require('../routes/index')
 const errorHandler = require('../middlewares/errorHandler')
-const connectDb = require('../config/dbConnection')
-const dotenv = require('dotenv')
+const connectDb = require('../db/dbConnection')
 
 const getApp = () => {
-  dotenv.config()
   connectDb()
   const app = express()
-  const port = process.env.PORT || 3000
 
   //middlewares
   app.use(express.json())
   app.use(router)
   app.use(errorHandler)
 
-  return { app, port }
+  return app
 }
 
 module.exports = getApp
