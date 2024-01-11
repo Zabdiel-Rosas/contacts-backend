435d6651f411dfa531de70a25185f4ea39678fd7
okami <zabdiel_rosas@outlook.com>
2024-01-10 19:44:12 -0600

    add(userRegister.js): validation library and handler are added

---
+++ middlewares/validationsHandler.js
@@ -0,0 +1,15 @@
+const validationsHandler = async (req, res, next, schema) => {
+  try {
+    const options = {
+      abortEarly: false,
+      convert: true,
+    }
+    await schema.validateAsync(req.body, options)
+    return next()
+  } catch (err) {
+    console.error(err)
+    return res.status(400).json(err)
+  }
+}
+
+module.exports = validationsHandler
