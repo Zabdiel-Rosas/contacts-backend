74f67b9eec92defa0240da2b71a3947adc7811e5
okami <zabdiel_rosas@outlook.com>
2024-01-12 21:31:35 -0600

    feat(userRoutes.js): validations are implemented

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,27 @@
-const express = require('express')
-const router = express.Router()
+const { Router } = require('express')
+const router = Router()
 const {
   userRegisterHandler,
   userLoginHandler,
   userCurrentHandler,
 } = require('../handlers/userHandlers')
+//Inputs Validations
 const registerUserValidation = require('../validations/userRegister')
+const loginUserValidation = require('../validations/userLogin')
+//Logic Validations
+const {
+  validateUserRegister,
+  validateUserLogin,
+} = require('../services/validateUserRoutes')
 const validateToken = require('../middlewares/validateTokenHandler')
 
-router.post('/register', registerUserValidation, userRegisterHandler)
-router.post('/login', userLoginHandler)
+router.post(
+  '/register',
+  registerUserValidation,
+  validateUserRegister,
+  userRegisterHandler
+)
+router.post('/login', loginUserValidation, validateUserLogin, userLoginHandler)
 router.get('/current', validateToken, userCurrentHandler)
 
 module.exports = router
