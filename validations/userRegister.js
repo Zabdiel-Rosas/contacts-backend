6e5fb86ea1feee3e31989de39b862a146610fc36
okami <zabdiel_rosas@outlook.com>
2024-01-12 20:17:18 -0600

    refactor(userRegister): removed logic validation from input validation

--- validations/userRegister.js
+++ validations/userRegister.js
@@ -1,17 +1,9 @@
 const joi = require('joi')
-const User = require('../models/userModel')
 const validationsHandler = require('../middlewares/validationsHandler')
-
-const isEmailAvailable = async (value, helpers) => {
-  const emailExist = await User.findOne({ email: value })
-  if (emailExist) {
-    return helpers.message('Email Already Exists!')
-  }
-}
 
 const schema = joi.object({
   username: joi.string().required().min(3).max(60),
-  email: joi.string().email().required().external(isEmailAvailable),
+  email: joi.string().email().required(),
   password: joi.string().required().pattern(new RegExp('^[a-zA-Z0-9]{8,64}$')),
   // repeatPassword: joi.ref('password'),
 })
