cc76b232d3c85ade093819412353f145d89104cd
okami <zabdiel_rosas@outlook.com>
2024-01-23 18:40:12 -0600

    refactor(errorHandler): now only handles server errors

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,10 +2,6 @@ const errorHandler = (err, req, res, next) => {
   const statusCode = err.status || 500
 
   const errorTitle = {
-    400: 'Validation Error',
-    401: 'Unauthorized',
-    403: 'Forbidden',
-    404: 'Not Found',
     500: 'Server Error',
   }
 
